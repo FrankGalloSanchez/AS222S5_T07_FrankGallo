316512f990064703758c04550ff891ddc4a877e2
Frank Sanchez <111780777+FrankGalloSanchez@users.noreply.github.com>
2024-10-17 08:29:53 -0500

    Update TranslatorTextRest.java

--- src/main/java/pe/edu/vallegrande/TranslatorText/rest/TranslatorTextRest.java
+++ src/main/java/pe/edu/vallegrande/TranslatorText/rest/TranslatorTextRest.java
@@ -73,7 +73,7 @@ public class TranslatorTextRest {
 				.defaultIfEmpty(ResponseEntity.notFound().build());
 	}
 
-	@GetMapping("/actives")
+	@GetMapping("/all/actives")
 	public Flux<Translation> getAllActives() {
 		return translatorTextService.findAllActives();
 	}
